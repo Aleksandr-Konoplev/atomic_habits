bff369aae999089b49c8622d03605d9d5b58a99c
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-04 13:48:40 +1000

    added caching password in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "email",
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    # Определяет группы полей и их порядок на странице редактирования существующего пользователя
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),                               # email и пароль
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),  # Права и роли пользователя
+        ('Important dates', {'fields': ('last_login',)}),                        # Дата последнего входа
     )
-    search_fields = ("email",)
-    ordering = ("email",)
+
+    # Определяет поля и оформление на форме создания нового пользователя
+    add_fieldsets = (
+        (
+            None,  # группа без заголовка
+            {
+                'classes': ('wide',),  # CSS-класс для широкого отображения формы
+                'fields': (
+                    'email',           # поле email
+                    'password1',       # пароль
+                    'password2',       # подтверждение пароля
+                    'is_staff',        # флаг персонала
+                    'is_active',       # флаг активности пользователя
+                ),
+            },
+        ),
+    )
