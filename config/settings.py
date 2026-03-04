1b778357bf72d6a8def9eb8bf7c27271ae444fb6
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-04 12:24:05 +1000

    add custom command dropcreatedb

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,12 @@ DEBUG = True if os.getenv('DEBUG') == 'True' else False
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Library
 
     # My app
@@ -27,33 +27,33 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = 'config.wsgi.application'
 
 # ----------------------------------------------------------------------------------
 # ---------------------------- Настройки базы данных -------------------------------
@@ -74,28 +74,28 @@ DATABASES = {
 # ------------------------- Валидаторы сложности пароля ----------------------------
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 # ----------------------- Модель аутентификации пользователя -----------------------
 AUTH_USER_MODEL = 'users.User'
 # ----------------------------------------------------------------------------------
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
---
+++ users/management/commands/dropcreatedb.py
@@ -0,0 +1,40 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Удаляет и пересоздаёт базу данных (только для dev)'
+
+    def handle(self, *args, **options):
+        db_name = settings.DATABASES['default']['NAME']
+        db_user = settings.DATABASES['default']['USER']
+        db_password = settings.DATABASES['default']['PASSWORD']
+        db_host = settings.DATABASES['default'].get('HOST', 'localhost')
+        db_port = settings.DATABASES['default'].get('PORT', 5432)
+
+        # Запрос подтверждения
+        confirm = input(f'Вы уверены, что хотите удалить базу "{db_name}" и создать заново? [y/N]: ')
+        if confirm.lower() != 'y':
+            self.stdout.write(self.style.WARNING('Операция отменена.'))
+            return
+
+        # Подключаемся к postgres (не к удаляемой базе)
+        conn = psycopg2.connect(dbname="postgres", user=db_user, password=db_password, host=db_host, port=db_port)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+
+        # Удаляем базу, если существует
+        self.stdout.write(f'Удаляем базу {db_name}...')
+        # noinspection SqlNoDataSourceInspection
+        cur.execute(f'DROP DATABASE IF EXISTS "{db_name}";')
+
+        # Создаём базу заново
+        self.stdout.write(f'Создаём базу {db_name}...')
+        # noinspection SqlNoDataSourceInspection
+        cur.execute(f'CREATE DATABASE "{db_name}";')
+
+        cur.close()
+        conn.close()
+        self.stdout.write(self.style.SUCCESS(f'База {db_name} успешно пересоздана!'))
