72678da3786a3b3e89c7d92004ee543444f72139
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-08 19:09:20 +1000

    implemented deferred tasks

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,9 @@ CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 CELERY_TIMEZONE = TIME_ZONE
 # CELERY_TASK_TRACK_STARTED = True
-# CELERY_TASK_TIME_LIMIT = 30 * 60
+
+# Лимит времени на выполнение задачи (в секундах)
+CELERY_TASK_TIME_LIMIT = 10 * 60
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
